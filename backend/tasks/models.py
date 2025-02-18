a9bf47df2bcbb51196211afb0a1d7254e186e768
tobit bushenyula <115138001+tobitdacoder@users.noreply.github.com>
2025-02-18 11:34:14 +0000

    ready for migration
    successfully initialized my newly added apps. next step is to run migration.

--- backend/tasks/models.py
+++ backend/tasks/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from users.models import CustomUser
 
+class Task(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    assigned_to = models.ForeignKey(CustomUser, on_delete=models)
 # Create your models here.
