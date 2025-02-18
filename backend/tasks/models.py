b54eac43c8514b96bab6ef8f1fd4261ca8349cd3
tobit bushenyula <115138001+tobitdacoder@users.noreply.github.com>
2025-02-18 11:47:14 +0000

    update before migration
    here I am setting my code for better migration.

--- backend/tasks/models.py
+++ backend/tasks/models.py
@@ -4,5 +4,6 @@ from users.models import CustomUser
 class Task(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
-    assigned_to = models.ForeignKey(CustomUser, on_delete=models)
+    assigned_to = models.ForeignKey(CustomUser, on_delete=models) # this did not work so I used AI to show me this down there WRITE IN CHALLENGE REPORT
+    # assigned_to = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
 # Create your models here.
